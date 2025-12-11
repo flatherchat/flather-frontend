e781df17268c01370f3f2699e82c22a27b674fa4
maxzieblox <hpounmyatmaung.personal@gmail.com>
2025-12-11 20:44:33 +0630

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	let userMessage: string = '';
+    let userName: string = '';
+
+    let websocketTarget: string = '2.56.246.119:30179';
+    let websocketStatus: string = '';
+    let ws: WebSocket;
+
+    let Conversation: string[] = [];
+
+    // kill me
+    function initWebsocket()
+    {
+        websocketStatus = 'Connecting...';
+        ws = new WebSocket(`ws://${websocketTarget}`);
+
+        if (!ws) {
+            return
+        }
+
+        ws.onclose = () => {
+            websocketStatus = "Connection Terminated. I'm sorry to interrupt you Elizabeth, if you still even remember that name. But I'm afraid you've been misinformed. You are not here to receive a gift, nor have you been called here by the individual you assume. Although you have indeed been called."
+        };
+
+        ws.onopen = () => {
+            websocketStatus = 'Connected successfuly!'
+        };
+
+        ws.onerror = (err) => {
+            websocketStatus = err.toString();
+        }
+
+        ws.onmessage = (msg) => {
+            Conversation = [...Conversation, msg.data];
+        }
+    }
+
+    function sendMessageHandler()
+    {
+        ws.send(`${userName}: ${userMessage}`)
+        userMessage = '';
+    }
+
+    initWebsocket();
+</script>
+
+<p>NOTE: This project is in early beta. Also you will lose the conversation history if you refresh. Good luck, and thank you for testing!</p>
+<p>{websocketStatus}</p>
+<div>
+    {#each Conversation as chat }
+        {chat}<br>
+    {/each}
+</div>
+<input bind:value={userName} placeholder='ur username here ig' type='text' required><br>
+<input bind:value={userMessage} placeholder='Send a message...' type='text' required><br>
+<button id='sendAction' on:click={sendMessageHandler}>Send</button>
